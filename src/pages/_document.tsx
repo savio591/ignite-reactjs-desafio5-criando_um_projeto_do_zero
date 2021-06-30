c0844eff0a4944a9abdc9620aa752a425953a563
savio591 <savio591@hotmail.com>
2021-06-30 20:39:20 -0300

    resolved document init

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,27 @@
-import Document from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="shortcut icon"
+            href="/images/favicon.png"
+            type="image/png"
+          />
+          <meta name="theme-color" content="#1a1d23" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
